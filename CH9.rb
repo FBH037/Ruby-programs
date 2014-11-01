f2c0148f04b58954b4c6f8bb9b28a438bb149953
FBH037 <fbhewitt@gmail.com>
2014-11-01 12:29:47 -0600

    CH9

--- CH9.rb
+++ CH9.rb
@@ -82,6 +82,55 @@
 
 
 
+# Defining a method
+# def say_moo
+# 	puts 'moooooo...'
+# end
+
+# say_moo
+# say_moo
+
+
+
+# #putting parameters on methods
+# def say_moo number_of_moos
+# 	puts 'moooooo...'*number_of_moos
+# end
+
+# say_moo 3
+# puts 'oink-oink'
+# say moo #should throw error because of missing parameter
+
+
+
+# #Local Variables only exist within the method
+# def double_this num
+# 	num_times_2 = num*2
+# 	puts num.to_s + ' doubled is ' + num_times_2.to_s
+# end
+
+# double_this 44
+
+
+
+# #Local variables don't carry into or out of method definitions
+# tough_var = 'You can\'t even touch my variable!'
+
+# def little_pest tough_var
+# 	tough_var = nil
+# 	puts 'HAHA! I ruined your variable!'
+# end
+
+# little_pest tough_var
+# puts tough_var
+
+
+
+
+
+
+
+
 
 
 
