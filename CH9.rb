dd93ded79372a99a1305079b409d1df7dea8f59c
FBH037 <fbhewitt@gmail.com>
2014-11-02 17:18:37 -0700

    CH9 EX

--- CH9.rb
+++ CH9.rb
@@ -126,11 +126,103 @@
 
 
 
-##puts puts?
-return_val = puts 'This puts returned:'
-puts return_val
+# #puts puts?
+# return_val = puts 'This puts returned:'
+# puts return_val
 
 
+
+# #local variable puts play
+# def say_moo number_of_moos
+# 	puts 'moooooo...'*number_of_moos
+# 	'yellow submarine'
+# end
+
+# x = say_moo 3
+# puts x.capitalize + ' , dude...'
+# puts x           + '.'
+
+
+
+# #Using explicit returns
+# def favorite_food name
+# 	if name == 'Lister'
+# 		return 'vindaloo'
+# 	end
+# 	if name == 'Rimmer'
+# 		return 'mashed potatoes'
+# 	end
+
+# 	'hard to say...maybe fried plantain?'
+# end
+
+# def favorite_drink name
+# 	if name == 'Jean-Luc'
+# 		'tea, Earl Grey, hot'
+# 	elsif name == 'Kathryn'
+# 		'coffee, black'
+# 	else
+# 		'perhaps...horchata?'
+# 	end
+# end
+
+# puts favorite_food('Rimmer')
+# puts favorite_food('Lister')
+# puts favorite_food('Cher')
+# puts favorite_drink('Kathryn')
+# puts favorite_drink('Oprah')
+# puts favorite_drink('Jean-Luc')
+
+
+
+#First program of this chapter revisited with custom methods
+def ask question
+	while true
+		puts question
+		reply = gets.chomp.downcase
+		if (reply == 'yes' || reply == 'no')
+			if reply == 'yes'
+				answer = true
+			else
+				answer = false
+			end
+			break
+		else
+			puts 'Please answer "yes" or "no".'
+		end
+	end
+
+	answer #This is what we return (tru or false)
+end
+
+puts 'Hello, and thank you for taking the time to'
+puts 'help me with this experiment. My experiment'
+puts 'has to do with the way people feel about'
+puts 'Mexican food. Just think about Mexican food'
+puts 'and try to answer every question honestly,'
+puts 'with either a "yes" or a "no". My experiment'
+puts 'has nothing to do with bed-wetting.'
+puts ''
+ask 'Do you like eating tacos?' 	
+ask 'Do you like eating burritos?'
+wets_bed = ask 'Do you wet the bed?' # ignores all return values expcept this one
+ask 'Do you like eating chimichangas?'
+ask 'Do you like eating sopapillas?'
+puts 'Just a few more questions...'
+ask 'Do you like drinking horchata?'
+ask 'Do you like eating flautas?'
+puts
+puts
+puts 'DEBRIEFING:'
+puts 'Thank you for taking the time to help with'
+puts 'this experiment. In fact, this expiremnt'
+puts 'has nothing to do with Mexican food. It is'
+puts 'an experiment about bed-wetting. The Mexican'
+puts 'food was just there to catch you off gaurd'
+puts 'in the hopes that you would answer more'
+puts 'honestly. Thanks again.'
+puts
+puts wets_bed
 
 
 
---
+++ CH9_EX9.5_Modern_RN.rb
@@ -0,0 +1,65 @@
+
+#Old school roman numerals method
+
+def Roman_Numeral number
+rn = []
+
+while number > 0
+	while (number/1000) >= 1
+		rn.push ('M')
+		number=number-1000	
+	end
+	while (number/500) >= 1
+		rn.push ('D')
+		number=number-500	
+	end
+	while (number/100) >= 1
+		rn.push ('C')
+		number=number-100	
+	end
+	while (number/50) >= 1
+		rn.push ('L')
+		number=number-50	
+	end
+	while (number/10) >= 1
+		rn.push ('X')
+		number=number-10	
+	end
+	if number == 4 
+		rn.push ('IV')
+		puts rn.join ('')
+	break
+	elsif number == 9
+		rn.push ('IX')
+		puts rn.join ('')
+	break
+	else
+
+		while (number/5) >= 1
+			rn.push ('V')
+			number=number-5	
+		end
+		while (number/1) >= 1
+			rn.push ('I')
+			number=number-1	
+		end
+
+	end
+	puts rn.join ('')
+	end
+end
+Roman_Numeral 4784
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
